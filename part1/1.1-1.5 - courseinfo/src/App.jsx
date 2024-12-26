819fb969483b96b59e1843401b67924c42a360bc
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2024-12-26 19:36:30 +0700

    Use single object
    Finish courseinfo

--- part1/1.1-1.5 - courseinfo/src/App.jsx
+++ part1/1.1-1.5 - courseinfo/src/App.jsx
@@ -13,8 +13,8 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      {props.parts.map((part, index) => (
-        <Part key={index} part={part.name} exercises={part.exercises} />
+      {props.parts.map((part) => (
+        <Part key={part.name} part={part.name} exercises={part.exercises} />
       ))}
     </div>
   )
@@ -26,27 +26,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
