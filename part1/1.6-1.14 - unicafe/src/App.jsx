c1f3d2ba490b7c7a107ddec8a43edc0143e6c431
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2024-12-29 13:26:58 +0700

    Add the buttons and statistics

--- part1/1.6-1.14 - unicafe/src/App.jsx
+++ part1/1.6-1.14 - unicafe/src/App.jsx
@@ -1,12 +1,34 @@
 import { useState } from 'react'
 
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const Statistic = ({ text, value }) => (
+  <div>
+    {text} {value}
+  </div>
+)
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  return <div>code here</div>
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={handleGood} text="good" />
+      <Button onClick={handleNeutral} text="neutral" />
+      <Button onClick={handleBad} text="bad" />
+
+      <h1>statistics</h1>
+      <Statistic text="good" value={good} />
+      <Statistic text="neutral" value={neutral} />
+      <Statistic text="bad" value={bad} />
+    </div>
+  )
 }
 
 export default App
