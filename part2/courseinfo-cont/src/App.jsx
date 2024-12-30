04c807fd56f483987a7d5bab21e3024d171bc125
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2024-12-30 18:33:49 +0700

    Add Course component
    Finish 2.1

--- part2/courseinfo-cont/src/App.jsx
+++ part2/courseinfo-cont/src/App.jsx
@@ -1,3 +1,26 @@
+const Header = ({ courseName }) => <h1>{courseName}</h1>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Course = ({ course }) => (
+  <div>
+    <Header courseName={course.name} />
+    <Content parts={course.parts} />
+  </div>
+)
+
 const App = () => {
   const course = {
     id: 1,
