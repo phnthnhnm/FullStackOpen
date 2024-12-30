97b6c14bf49f4c189068f299c93c055fe555eeea
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2024-12-30 18:35:38 +0700

    Add total counter
    Finish 2.2

--- part2/courseinfo-cont/src/App.jsx
+++ part2/courseinfo-cont/src/App.jsx
@@ -14,10 +14,20 @@ const Content = ({ parts }) => (
   </div>
 )
 
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return (
+    <p>
+      <b>total of {total} exercises</b>
+    </p>
+  )
+}
+
 const Course = ({ course }) => (
   <div>
     <Header courseName={course.name} />
     <Content parts={course.parts} />
+    <Total parts={course.parts} />
   </div>
 )
 
