cf43af1abd4ba8ba452659887ba355267cf868c9
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2024-12-31 14:57:16 +0700

    Save numbers to server
    Finish 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -26,9 +26,11 @@ const App = () => {
         name: newName,
         number: newNumber,
       }
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      axios.post('http://localhost:3001/persons', personObject).then((response) => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
     }
   }
 
