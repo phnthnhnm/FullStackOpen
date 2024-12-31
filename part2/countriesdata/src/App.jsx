82ada5c68fa96f7d78a55865ee490e0504272622
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2024-12-31 15:51:29 +0700

    Display list of requested countries (10 <= results)

--- part2/countriesdata/src/App.jsx
+++ part2/countriesdata/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react'
+import countriesService from './services/countries'
+import CountryList from './components/CountryList'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [search, setSearch] = useState('')
+  const [filteredCountries, setFilteredCountries] = useState([])
+
+  useEffect(() => {
+    countriesService.getAll().then((initialCountries) => {
+      setCountries(initialCountries)
+    })
+  }, [])
+
+  useEffect(() => {
+    setFilteredCountries(countries.filter((country) => country.name.common.toLowerCase().includes(search.toLowerCase())))
+  }, [search, countries])
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value)
+  }
 
   return (
-    <>
+    <div>
+      <h1>Country Information</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Find countries: <input value={search} onChange={handleSearchChange} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      {filteredCountries.length > 10 && <p>Too many matches, specify another filter</p>}
+      {filteredCountries.length <= 10 && <CountryList countries={filteredCountries} />}
+    </div>
   )
 }
 
