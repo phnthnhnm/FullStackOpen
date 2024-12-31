96d4c4ad3c6e4b7eb9fd747c2e72eb9fe12332bd
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2024-12-31 21:27:40 +0700

    Add a getById api
    Finish 3.3

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find((person) => person.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
 app.get('/info', (req, res) => {
   const entryCount = persons.length
   const requestTime = new Date()
