f528d5cbf3692704637fe0554cd0e9030e65e821
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2024-12-31 15:59:31 +0700

    Add a show button to each country when there are multiple matches
    Finish 2.19

--- part2/countriesdata/src/App.jsx
+++ part2/countriesdata/src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
   const [countries, setCountries] = useState([])
   const [search, setSearch] = useState('')
   const [filteredCountries, setFilteredCountries] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState(null)
 
   useEffect(() => {
     countriesService.getAll().then((initialCountries) => {
@@ -20,6 +21,11 @@ const App = () => {
 
   const handleSearchChange = (event) => {
     setSearch(event.target.value)
+    setSelectedCountry(null)
+  }
+
+  const handleShowCountry = (country) => {
+    setSelectedCountry(country)
   }
 
   return (
@@ -29,8 +35,9 @@ const App = () => {
         Find countries: <input value={search} onChange={handleSearchChange} />
       </div>
       {filteredCountries.length > 10 && <p>Too many matches, specify another filter</p>}
-      {filteredCountries.length <= 10 && filteredCountries.length > 1 && <CountryList countries={filteredCountries} />}
+      {filteredCountries.length <= 10 && filteredCountries.length > 1 && <CountryList countries={filteredCountries} onShowCountry={handleShowCountry} />}
       {filteredCountries.length === 1 && <CountryDetails country={filteredCountries[0]} />}
+      {selectedCountry && <CountryDetails country={selectedCountry} />}
     </div>
   )
 }
--- part2/countriesdata/src/components/CountryList.jsx
+++ part2/countriesdata/src/components/CountryList.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const CountryList = ({ countries }) => {
+const CountryList = ({ countries, onShowCountry }) => {
   return (
     <ul>
       {countries.map((country) => (
-        <li key={country.cca3}>{country.name.common}</li>
+        <li key={country.cca3}>
+          {country.name.common} <button onClick={() => onShowCountry(country)}>show</button>
+        </li>
       ))}
     </ul>
   )
