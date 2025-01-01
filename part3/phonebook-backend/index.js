c3d4f91b0b42c6c2cb9153d9cc72083cb5b9ac7b
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-01-01 11:40:56 +0700

    Use dotenv

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(cors())
 app.use(express.json())
-// app.use(express.static('dist'))
 
 // Custom token to log request body for POST requests
 morgan.token('body', (req) => JSON.stringify(req.body))
@@ -13,15 +14,10 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 // Use morgan with custom format to include request body
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-const persons = [
-  { id: 1, name: 'Arto Hellas', number: '040-123456' },
-  { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
-  { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
-  { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' },
-]
-
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then((persons) => {
+    res.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (req, res) => {
@@ -78,7 +74,7 @@ app.get('/info', (req, res) => {
   )
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
