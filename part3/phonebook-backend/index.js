dcbffd6722596502f32aac097235729825946e55
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-01-01 12:34:38 +0700

    Lint all .js files
    Finish 3.22

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -69,7 +69,7 @@ app.put('/api/persons/:id', (req, res, next) => {
 
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
-    .then((result) => {
+    .then(() => {
       res.status(204).end()
     })
     .catch((error) => next(error))
