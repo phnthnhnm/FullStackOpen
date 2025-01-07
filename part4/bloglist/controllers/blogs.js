f0dd2dc0a51125c64d031e760a4d8d66224455cc
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-01-07 17:06:34 +0700

    Pass get /api/blogs test
    Finish 4.8

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,13 +1,12 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (req, res) => {
-  Blog.find({}).then((blogs) => {
-    res.json(blogs)
-  })
+blogsRouter.get('/', async (req, res) => {
+  const blogs = await Blog.find({})
+  res.json(blogs)
 })
 
-blogsRouter.post('/', (req, res, next) => {
+blogsRouter.post('/', async (req, res) => {
   const body = req.body
 
   const blog = new Blog({
@@ -17,12 +16,8 @@ blogsRouter.post('/', (req, res, next) => {
     likes: body.likes,
   })
 
-  blog
-    .save()
-    .then((savedBlog) => {
-      res.json(savedBlog)
-    })
-    .catch((error) => next(error))
+  const savedBlog = await blog.save()
+  res.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,25 @@
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const helper = require('./test_helper')
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
