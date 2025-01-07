aebc68331d274dd58d35ae34f69cefa491c19904
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-01-07 17:08:20 +0700

    Pass id verification test
    Finish 4.9

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,11 +1,11 @@
 const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 
 const helper = require('./test_helper')
-
 const Blog = require('../models/blog')
 
 beforeEach(async () => {
@@ -18,6 +18,16 @@ test('blogs are returned as json', async () => {
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+  const blogs = response.body
+
+  blogs.forEach((blog) => {
+    assert(blog.id)
+    assert.strictEqual(blog._id, undefined)
+  })
 })
 
 after(async () => {
