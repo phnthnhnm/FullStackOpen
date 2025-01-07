b0b8e6699e395c292e463723e1cb21d8dfde31e8
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-01-07 15:39:47 +0700

    Move schema definition to blog.js

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,19 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
+const Blog = require('./models/blog')
 
 app.use(cors())
 app.use(express.json())
