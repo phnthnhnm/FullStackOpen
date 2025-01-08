0811d3f691b2d0f5be90c8756d5ff03e24ea7d04
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-01-08 12:58:41 +0700

    Add GET user api
    Finish 4.15

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -2,8 +2,13 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.post('/', async (request, response) => {
-  const { username, name, password } = request.body
+usersRouter.get('/', async (req, res) => {
+  const users = await User.find({})
+  res.json(users)
+})
+
+usersRouter.post('/', async (req, res) => {
+  const { username, name, password } = req.body
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
@@ -16,7 +21,7 @@ usersRouter.post('/', async (request, response) => {
 
   const savedUser = await user.save()
 
-  response.status(201).json(savedUser)
+  res.status(201).json(savedUser)
 })
 
 module.exports = usersRouter
