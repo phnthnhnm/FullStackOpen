0b7556016b48f4852016d5348f9106802586b23c
Phan Thành Nam <53371769+phnthnhnm@users.noreply.github.com>
2025-01-08 13:22:24 +0700

    Move token extraction to middleware
    Finish 4.20

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -26,8 +26,19 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    req.token = authorization.replace('Bearer ', '')
+  } else {
+    req.token = null
+  }
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
 }
